7f03e29b9b44377b3c5d81a753b8e253555eb1b0
daiphung <daiphung.hd@gmail.com>
2026-06-22 19:19:39 +0700

    config WebClientConfig kết nối

---
+++ promotion-service/src/main/java/org/example/promotionservice/PromotionServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.promotionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromotionServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PromotionServiceApplication.class, args);
+    }
+
+}
